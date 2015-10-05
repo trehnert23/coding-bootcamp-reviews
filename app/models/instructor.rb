5ead680b56eb0a818fdfb035ef43fc6fed2a3ee2
Eric Euresti <eeuresti@gmail.com>
2015-10-05 13:40:53 -0700

    working on searchbar

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -3,4 +3,12 @@ class Instructor < ActiveRecord::Base
 	has_many :reviews
 	belongs_to :bootcamp
 
+	def self.search(search)
+	  if search
+	    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+	  else
+	    find(:all)
+	  end
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	root to: 'welcome#index'
 
 	post '/users', to: 'users#create'
+	get '/instructors', to: 'instructors#index'
 
 end
