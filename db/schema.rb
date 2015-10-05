ad87cc95e1340ba84bbdd2a23c5a1a7b9e4be821
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-05 12:28:26 -0700

    added bootcamp_id column to bootcamp_review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005190757) do
+ActiveRecord::Schema.define(version: 20151005191603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20151005190757) do
     t.string   "worthit"
     t.integer  "location"
     t.integer  "jobhelp"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bootcamp_id"
   end
 
   create_table "bootcamps", force: :cascade do |t|
