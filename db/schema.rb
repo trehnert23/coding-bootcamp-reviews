8fcf28f02b9c72a0402dbae9434492b4ee14405d
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-10-06 14:44:16 -0700

    search bars in place

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20151006184214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "bootcamp_reviews", force: :cascade do |t|
     t.text     "content"
     t.integer  "campus"
