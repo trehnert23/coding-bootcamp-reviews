039e0f23d9c9057c352d1b04fa65fc4463d66c34
Thomas M. Rehnert <tomrehnert2011@gmail.com>
2016-02-17 17:05:57 -0500

    still trying

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,6 +22,6 @@ class SessionsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    # params.require(:user).permit(:email, :password)
   end
 end
